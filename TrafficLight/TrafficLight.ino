90cece54992bf0a46fb9acc7352a85d180609938
Joshua Mitchell <97917260+longsundaywalks@users.noreply.github.com>
2023-03-28 12:29:17 +0100

    Add files via upload

---
+++ TrafficLight/TrafficLight.ino
@@ -0,0 +1,16 @@
+/*Refer to .S Traffic Light Code
+
+To correctly use this code, run the .S file (TrafficLight.S)
+
+Ground a digital wire to the negative row of a breadboard.
+
+Connect resistors to the cathode leg (shorter) of LEDs and another wire to the anode (longer) leg of the LED
+
+For assembly, you will be using digital pins 13 - 10 and 50 - 53 using PORT B (DDRB)
+
+Once these are all connected, you can run the program and it should function like a traffic light with
+pedestrian lights.
+
+
+
+Author: Joshua Mitchell*/
